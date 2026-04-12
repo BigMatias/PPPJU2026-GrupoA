56ee52398e84cf2f0305f407213be1643b67e294
Facundo Gerry <facuto365pro@gmail.com>
2026-04-12 18:44:52 -0300

    Add scriptable objects (soundData, gameData, runData)

---
+++ Assets/Scripts/Data/RunDataSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RunData", menuName = "GameSettings/RunData")]
+
+public class RunDataSO : ScriptableObject
+{
+    public int totalHands;
+    public int handsPlayed;
+}
