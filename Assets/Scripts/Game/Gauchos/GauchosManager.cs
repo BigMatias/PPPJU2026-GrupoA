0ab59042a3a4c9a727f3dd03988d2833f74c7dc2
Facundo Gerry <facuto365pro@gmail.com>
2026-05-18 10:18:36 -0300

    Add Get and Set methods to GauchosManager

--- Assets/Scripts/Game/Gauchos/GauchosManager.cs
+++ Assets/Scripts/Game/Gauchos/GauchosManager.cs
@@ -5,11 +5,32 @@ using UnityEngine;
  * Este script es el que se encarga de callear a los gauchos activos, que estarían en la lista _activeGauchos}
  * Se usan 2 foreach, el primero recorre la lista
  * El 2do recorre la lista de efectos del propio gaucho
+ * Le agregué unos métodos de get por si necesitamos usarlos
+ * Además, le puse el SetNewGaucho para poder agregar los que se compren a la lista, sino ninguno andaría jeje
  */
 
 public class GauchosManager : MonoBehaviour
 {
     private List<GauchoInstance> _activeGauchos = new List<GauchoInstance>();
+
+    public void SetNewGaucho(GauchoInstance instance)
+    {
+        _activeGauchos.Add(instance);
+    }
+
+    public GauchoInstance GetGaucho(GauchoInstance instance)
+    {
+        foreach (GauchoInstance item in _activeGauchos)
+            if (instance == item)
+                return item;
+
+        return null;
+    }
+
+    public List<GauchoInstance> GetGaucho()
+    {
+        return _activeGauchos;
+    }
 
     public void Trigger(GameEvents gameEvents, GauchoContext context)
     {
