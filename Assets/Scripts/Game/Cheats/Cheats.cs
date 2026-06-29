ed4b15bb35936f6d2035cd766f12c4a9fe305f86
Facundo Gerry <facuto365pro@gmail.com>
2026-06-29 11:31:34 -0300

    Add Cheats.cs

---
+++ Assets/Scripts/Game/Cheats/Cheats.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Cheats : MonoBehaviour
+{
+    [SerializeField] private KeyCode _keyWin;
+    [SerializeField] private KeyCode _keyLose;
+    [SerializeField] private KeyCode _keyMoney;
+
+    private void Update()
+    {
+        if (!RunManager.Instance) return;
+
+        if (Input.GetKeyDown(_keyWin))
+            WinPressed();
+        if (Input.GetKeyDown(_keyLose))
+            LosePressed();
+        if (Input.GetKeyDown(_keyMoney))
+            MoneyPressed();
+    }
+
+    private void WinPressed()
+    {
+    }
+
+    private void LosePressed()
+    {
+
+    }
+
+    private void MoneyPressed()
+    {
+
+    }
+}
